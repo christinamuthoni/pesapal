e824be757b0fa5259b282bb54c92a687b65cd427
caroline <christine.muriungi@student.moringaschool.com>
2024-04-25 09:23:01 +0300

    challenge1

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+def printif(condition, message):
+    if condition:
+        print(message)
+    else:
+        print('wrong data')
+
+#example usage
+
+y = 2
+printif(y> 4, 'y is greater than 4')
+
